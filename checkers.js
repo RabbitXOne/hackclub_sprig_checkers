407334c50cec6183c75b53f543b6431a8a9616a7
RabbitXOne <rabbitxones@googlemail.com>
2024-06-28 12:04:40 +0200

    Slowly adding basic features ✅

--- checkers.js
+++ checkers.js
@@ -14,10 +14,12 @@ const redChecker = "r"
 const whiteChecker = "w"
 const selectedRed = "p"
 const selectedWhite = "o"
-const selectedE1 = "i"
-const selectedE2 = "t"
+const selectedE1N = "i"
+const selectedE2N = "t"
+const selectedRedN = "e"
+const selectedWhiteN = "f"
 
-// const turn = "w"
+var turn = "w"
 
 setLegend(
   [emptyField1, bitmap`
@@ -122,40 +124,74 @@ CCCCCCCCCCCCCCCC`],
 4CCCC222222CCCC4
 4CCCCCCCCCCCCCC4
 4444444444444444`],
-  [selectedE1, bitmap`
-4444444444444444
-4999999999999994
-4999999999999994
-4999999999999994
-4999999999999994
-4999999999999994
-4999999999999994
-4999999999999994
-4999999999999994
-4999999999999994
-4999999999999994
-4999999999999994
-4999999999999994
-4999999999999994
-4999999999999994
-4444444444444444`],
-  [selectedE2, bitmap`
-4444444444444444
-4CCCCCCCCCCCCCC4
-4CCCCCCCCCCCCCC4
-4CCCCCCCCCCCCCC4
-4CCCCCCCCCCCCCC4
-4CCCCCCCCCCCCCC4
-4CCCCCCCCCCCCCC4
-4CCCCCCCCCCCCCC4
-4CCCCCCCCCCCCCC4
-4CCCCCCCCCCCCCC4
-4CCCCCCCCCCCCCC4
-4CCCCCCCCCCCCCC4
-4CCCCCCCCCCCCCC4
-4CCCCCCCCCCCCCC4
-4CCCCCCCCCCCCCC4
-4444444444444444`]
+  [selectedE1N, bitmap`
+DDDDDDDDDDDDDDDD
+D99999999999999D
+D99999999999999D
+D99999999999999D
+D99999999999999D
+D99999999999999D
+D99999999999999D
+D99999999999999D
+D99999999999999D
+D99999999999999D
+D99999999999999D
+D99999999999999D
+D99999999999999D
+D99999999999999D
+D99999999999999D
+DDDDDDDDDDDDDDDD`],
+  [selectedE2N, bitmap`
+DDDDDDDDDDDDDDDD
+DCCCCCCCCCCCCCCD
+DCCCCCCCCCCCCCCD
+DCCCCCCCCCCCCCCD
+DCCCCCCCCCCCCCCD
+DCCCCCCCCCCCCCCD
+DCCCCCCCCCCCCCCD
+DCCCCCCCCCCCCCCD
+DCCCCCCCCCCCCCCD
+DCCCCCCCCCCCCCCD
+DCCCCCCCCCCCCCCD
+DCCCCCCCCCCCCCCD
+DCCCCCCCCCCCCCCD
+DCCCCCCCCCCCCCCD
+DCCCCCCCCCCCCCCD
+DDDDDDDDDDDDDDDD`],
+  [selectedRedN, bitmap`
+DDDDDDDDDDDDDDDD
+DCCCCCCCCCCCCCCD
+DCCCC333333CCCCD
+DCC3333333333CCD
+DCC3333333333CCD
+DC333333333333CD
+DC333333333333CD
+DC333333333333CD
+DC333333333333CD
+DC333333333333CD
+DC333333333333CD
+DCC3333333333CCD
+DCC3333333333CCD
+DCCCC333333CCCCD
+DCCCCCCCCCCCCCCD
+DDDDDDDDDDDDDDDD`],
+  [selectedWhiteN, bitmap`
+DDDDDDDDDDDDDDDD
+DCCCCCCCCCCCCCCD
+DCCCC222222CCCCD
+DCC2222222222CCD
+DCC2222222222CCD
+DC222222222222CD
+DC222222222222CD
+DC222222222222CD
+DC222222222222CD
+DC222222222222CD
+DC222222222222CD
+DCC2222222222CCD
+DCC2222222222CCD
+DCCCC222222CCCCD
+DCCCCCCCCCCCCCCD
+DDDDDDDDDDDDDDDD`]
 )
 
 // setSolids([])
@@ -170,7 +206,7 @@ ldldldld
 dldldldl
 lwlwlwlw
 wlwlwlwl
-lwlwlwlw`
+lolwlwlw`
 ]
 
 setMap(levels[level])
@@ -182,18 +218,75 @@ setMap(levels[level])
 
 onInput("w", () => {
 
-  // if (!getAll(selectedWhite).length > 0) {
+  if(turn === "w") {
 
-  //   // if() {}
-  //   clearTile(0, 7)
-  //   addSprite(0, 7, selectedWhite)
+    // if (!getAll(selectedWhite).length > 0) {
+    //   // ERROR
+    // }
 
-  // } else {
-  //   // Move selectedRed one block upwards
-  // }
+    var currentSprite = ""
+    
+    if(getFirst(selectedWhite)) {
+      currentSprite = selectedWhite
+    } else if(getFirst(selectedRedN)) {
+      currentSprite = selectedRedN
+    } else if(getFirst(selectedE1N)) {
+      currentSprite = selectedE1N
+    } else if(getFirst(selectedE2N)) {
+      currentSprite = selectedE2N
+    }
+    
+    const currentX = getFirst(currentSprite).x
+    const currentY = getFirst(currentSprite).y
+
+    // Rewrite this to switch-case method?
+    
+    // FROM: selectedWhite
+    if(currentSprite === selectedWhite) {
+      // TO: emptyField1
+      if(getTile(currentX, currentY-1).some(sprite => sprite.type === emptyField1)) {
+        clearTile(currentX, currentY-1)
+        clearTile(currentX, currentY)
+        addSprite(currentX, currentY-1, selectedE1N)
+        addSprite(currentX, currentY, whiteChecker)
+        
+        // TO: emptyField2
+      } else if(getTile(currentX, currentY-1).some(sprite => sprite.type === emptyField2)) {
+        clearTile(currentX, currentY-1)
+        clearTile(currentX, currentY)
+        addSprite(currentX, currentY-1, selectedE2N)
+        addSprite(currentX, currentY, whiteChecker)
+
+        // TO: redChecker
+      } else if(getTile(currentX, currentY-1).some(sprite => sprite.type === redChecker)) {
+        clearTile(currentX, currentY-1)
+        clearTile(currentX, currentY)
+        addSprite(currentX, currentY-1, selectedRedN)
+        addSprite(currentX, currentY, whiteChecker)
+
+        // TO: whiteChecker
+      } else if(getTile(currentX, currentY-1).some(sprite => sprite.type === redChecker)) {
+        clearTile(currentX, currentY-1)
+        clearTile(currentX, currentY)
+        addSprite(currentX, currentY-1, selectedWhite)
+        addSprite(currentX, currentY, whiteChecker)
+      }
+    }
+    
+    
+    
+    
+    
+  }
+  
+  
 
 })
 
 afterInput(() => {
-
+  // if(turn === "w") {
+  //   turn = "r"
+  // } else if(turn === "r") {
+  //   turn = "w"
+  // }
 })
